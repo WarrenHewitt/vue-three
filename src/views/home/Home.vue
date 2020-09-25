ae5e257d5e615f4e3a10c93342910923252a8cb0
hewei <hewei@gmail.com>
2020-09-25 17:22:55 +0800

    regular update

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
@@ -10,9 +10,9 @@ import { Options, Vue } from 'vue-class-component'
 import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 @Options({
-  components: {
-    HelloWorld
-  }
+    components: {
+        HelloWorld
+    }
 })
 export default class Home extends Vue {}
 </script>
